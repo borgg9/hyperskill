b667b3d75b8cd2c71bf9e4e7585850fd5f692828
borgg9 <52835335+borgg9@users.noreply.github.com>
2020-06-20 17:54:48 +0200

    Delete Chatty_Bot_2_5.py

--- Chatty_Bot_2_5.py
+++
@@ -1,7 +0,0 @@
-print('Hello! My name is Aid.')
-print('I was created in 2020.')
-print('Please, remind me your name.')
-your_name = input()
-# reading a name
-
-print('What a great name you have, {}!'.format(your_name))
